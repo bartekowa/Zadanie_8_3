bc61d37ff4610a7bc9e10d2dcfeeeeaa891ae96b
Bartłomiej Kowalówka <bartekowa@gmail.com>
2017-04-19 18:24:00 +0200

    3 comments added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 // plik scripts.js
 
 var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
+alert('Witaj, ' + name); // tutaj jest komentarz do linii z poleceniem alert
 console.log('Witaj, ' + name);
+
+//pierwszy nowy komentarz
+/* drugi nowy komentarz */
